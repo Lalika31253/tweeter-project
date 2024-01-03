52f4f416d568021d695051c11543a82734d45135
Lalika31253 <Lalika31253@gmail.com>
2024-01-02 23:10:11 -0700

    Add responsive design for different screen rosolutions, add a project screenshots

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Tweeter Project
 
-Tweeter is a simple, single-page Twitter clone.
+Tweeter is a simple, single-page Twitter clone. It is used to share information quickly.
 
-This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express back-end skills.
+This project was build as part of the learning process at Lighthouse Labs, focusing on practicing variouse front-end skills such as HTML, CSS, JS, jQuery and AJAX, as well as back-end skills using Node and Express.
+
 
 ## Getting Started
 
-1. [Create](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) a new repository using this repository as a template.
-2. Clone your repository onto your local device.
-3. Install dependencies using the `npm install` command.
+1. Clone this repository (https://github.com/Lalika31253/tweeter-project) onto your local device.
+2. Install dependencies using the `npm install` command.
 3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
 4. Go to <http://localhost:8080/> in your browser.
 
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
     <div class="upper-row">
 
      <div class="tweet-header">
-       <i class="fa-solid fa-user-nurse fa-xl"></i>
+       <img src='${data.user.avatars}'></i>
        <p>${data.user.name}</p>
      </div>
     
@@ -83,13 +83,6 @@ $(document).ready(function () {
     };
   };
 
-  //hendle error mesage
-  setTimeout(() => {
-    $('.error-message').fadeOut('slow');
-  }, 2000);
-
-  $('.error-message').empty();
-
 
   // grab a form
   const $form = $('#new-tweet-item');
@@ -104,23 +97,26 @@ $(document).ready(function () {
     //get tweet value
     const $tweetText = $('#tweet-text').val();
 
+    //hendle error mesage
+    setTimeout(() => {
+      $('.error-message').fadeOut('slow');
+    }, 2000);
+
+    $('.error-message').empty();
+
     //check for user input
     if ($tweetText === null || $tweetText === "") {
       // alert("You can not post an empty tweet!");
-      $(".error-message").append('<i class="fa-solid fa-triangle-exclamation error-message"></i> You can not post an empty tweet! <i class="fa-solid fa-triangle-exclamation error-message"></i>').slideDown();
+      $(".error-message").append('<i class="fa-solid fa-triangle-exclamation"></i> You can not post an empty tweet! <i class="fa-solid fa-triangle-exclamation"></i>').slideDown();
     } else if ($tweetText.length > 140) {
       // alert("Your tweet is to big! The text size should't exceed 140 characters");
-      $(".error-message").append('<i class="fa-solid fa-triangle-exclamation error-message"></i> Your tweet is to big! The text size should not exceed 140 characters! <i class="fa-solid fa-triangle-exclamation error-message"></i>').slideDown();
+      $(".error-message").append('<i class="fa-solid fa-triangle-exclamation"></i> Your tweet is to long! The text size should not exceed 140 characters! <i class="fa-solid fa-triangle-exclamation"></i>').slideDown();
     } else {
       //send POST request
       $.ajax({
         method: 'POST',
         url: '/tweets',
         data: $formData,
-        // succss: () => {
-        //   console.log("Post request resolved successfully");
-        //   //make a GET request to retrieve all tweet items
-        //   loadTweets();
       }).then(() => {
         $('#tweet-text').val('');
         $('#counter').val('140');
